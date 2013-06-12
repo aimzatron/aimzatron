9fdfc91260d472b13eb5c824e1a9551628643e06
Aimee Maher <aimee@geekycheeks.com>
2013-06-12 17:18:20 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aimzatron::Application.config.secret_token = '0ec9c9214c40c0e6fed2bc5d89a2b7009d9bb2eccb9dd05df4aa039b76e5cd07d9e0a37e94dacfed52a95dec64b789274c7a6ae169c083ae1f7ceb8267df32c6'
